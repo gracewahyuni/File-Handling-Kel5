5169da56bf0a9faf0b40292ec305a5d302f42b6e
gracewahyuni <gracewahyuni06@student.ub.ac.id>
2025-04-29 19:22:23 +0700

    update

--- file-handling/logic.php
+++ file-handling/logic.php
@@ -1,21 +1,18 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
-    // Ambil data tag dari form
+    
     $tagAsal = trim($_POST['tag_asal']);
     $tagBaru = trim($_POST['tag_baru']);
     $tipeKeluaran = strtoupper($_POST['output_type']);
 
-    // Pastikan file berhasil diunggah
     if (isset($_FILES['uploaded_file']) && $_FILES['uploaded_file']['error'] === UPLOAD_ERR_OK) {
         $tmpName = $_FILES['uploaded_file']['tmp_name'];
         $originalName = $_FILES['uploaded_file']['name'];
         $html = file_get_contents($tmpName);
 
-        // Ganti tag
         $html = str_replace("<$tagAsal>", "<$tagBaru>", $html);
         $html = str_replace("</$tagAsal>", "</$tagBaru>", $html);
 
-        // Proses penyimpanan berdasarkan output_type
         if ($tipeKeluaran === "O") {
             file_put_contents($originalName, $html);
             echo "<p style='color:green;'>Tag berhasil diubah dan disimpan dengan file yang sama dengan nama $originalName</p>";
